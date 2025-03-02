0d27020c937f766513602ce8963cd42097d0cfa7
Mohammed Abba Aji <ajinote9@gmail.com>
2025-03-02 11:43:35 +0100

    Added models.py file

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
